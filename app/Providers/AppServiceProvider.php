41f2ac57ff3d2be9b428fe5095b7779b52f8463e
reyatwel <sysdev@gmx.com>
2024-11-09 17:41:41 +0800

    refactor global settings

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -44,44 +44,44 @@ class AppServiceProvider extends ServiceProvider
                 $logo_setting = LogoSetting::first();
                 $mailSetting = EmailConfiguration::first();
                 $pusherSetting = PusherSetting::first();
-
-                if ($general_setting && $logo_setting && $mailSetting && $pusherSetting) {
-                    /**
-                     * Set Timezone
-                     */
-                    Config::set('app.timezone', $general_setting->timezone);
-
-                    /** Set Mail Config */
-                    Config::set('mail.mailers.smtp.host', $mailSetting->host);
-                    Config::set('mail.mailers.smtp.port', $mailSetting->port);
-                    Config::set('mail.mailers.smtp.encryption', $mailSetting->encryption);
-                    Config::set('mail.mailers.smtp.username', $mailSetting->username);
-                    Config::set('mail.mailers.smtp.password', $mailSetting->password);
-
-                    /** Set Broadcasting Config */
-                    Config::set('broadcasting.connections.pusher.key', $pusherSetting->pusher_key);
-                    Config::set('broadcasting.connections.pusher.secret', $pusherSetting->pusher_secret);
-                    Config::set('broadcasting.connections.pusher.app_id', $pusherSetting->pusher_app_id);
-                    Config::set(
-                        'broadcasting.connections.pusher.options.host',
-                        "api-" . $pusherSetting->pusher_cluster . ".pusher.com"
-                    );
-
-                    /**
-                     * Access settings at all views
-                     */
-                    View::composer('*', static function ($view) use ($general_setting, $logo_setting, $pusherSetting) {
-                        $view->with([
-                            'settings' => $general_setting,
-                            'logo_setting' => $logo_setting,
-                            'pusherSetting' => $pusherSetting
-                        ]);
-                    });
-                }
             }
         } catch (\Exception $e) {
             // Log error if needed but don't throw exception
             // \Log::error($e->getMessage());
+        }
+
+        if ($general_setting && $logo_setting && $mailSetting && $pusherSetting) {
+            /**
+             * Set Timezone
+             */
+            Config::set('app.timezone', $general_setting->timezone);
+
+            /** Set Mail Config */
+            Config::set('mail.mailers.smtp.host', $mailSetting->host);
+            Config::set('mail.mailers.smtp.port', $mailSetting->port);
+            Config::set('mail.mailers.smtp.encryption', $mailSetting->encryption);
+            Config::set('mail.mailers.smtp.username', $mailSetting->username);
+            Config::set('mail.mailers.smtp.password', $mailSetting->password);
+
+            /** Set Broadcasting Config */
+            Config::set('broadcasting.connections.pusher.key', $pusherSetting->pusher_key);
+            Config::set('broadcasting.connections.pusher.secret', $pusherSetting->pusher_secret);
+            Config::set('broadcasting.connections.pusher.app_id', $pusherSetting->pusher_app_id);
+            Config::set(
+                'broadcasting.connections.pusher.options.host',
+                "api-" . $pusherSetting->pusher_cluster . ".pusher.com"
+            );
+
+            /**
+             * Access settings at all views
+             */
+            View::composer('*', static function ($view) use ($general_setting, $logo_setting, $pusherSetting) {
+                $view->with([
+                    'settings' => $general_setting,
+                    'logo_setting' => $logo_setting,
+                    'pusherSetting' => $pusherSetting
+                ]);
+            });
         }
     }
 
